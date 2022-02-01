c7293a1499fb4becb6d1b89d6d367955956f4994
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-01 21:20:45 +0100

    8. Developing Top Down Web Services Creating the CustomerOrders Service ...

---
+++ 8. Developing Top Down Web Services/src/main/java/com/cleverdeveloper/ws/soap/topdown/CustomerOrdersWebServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cleverdeveloper.ws.soap.topdown;
+
+/*
+PROJECT NAME : 8. Developing Top Down Web Services
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 01/02/2022 21:18
+*/
+
+import com.cleverdeveloper.ws.trainings.*;
+
+public class CustomerOrdersWebServiceImpl implements CustomerOrdersPortType {
+    @Override
+    public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
+        return null;
+    }
+
+    @Override
+    public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+        return null;
+    }
+}
