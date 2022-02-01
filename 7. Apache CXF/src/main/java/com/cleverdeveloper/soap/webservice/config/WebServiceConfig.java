ad51e3d7c587c9b329483447e54c44d5db8ece4d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-01 12:46:56 +0100

    7. Apache CXF Creating the configuration class

---
+++ 7. Apache CXF/src/main/java/com/cleverdeveloper/soap/webservice/config/WebServiceConfig.java
@@ -0,0 +1,33 @@
+package com.cleverdeveloper.soap.webservice.config;
+
+/*
+PROJECT NAME : 7. Apache CXF
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 01/02/2022 12:32
+*/
+
+import com.cleverdeveloper.soap.webservice.HelloWorldWebService;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.ws.Endpoint;
+
+@Configuration
+public class WebServiceConfig {
+
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Endpoint endpoint() {
+        // Make sure that you're importing the right Endpoint class
+        Endpoint endpoint = new EndpointImpl(bus, new HelloWorldWebService());
+        // this endpoint is available at the particular URL : /hello
+        endpoint.publish("/hello");
+        return endpoint;
+    }
+}
