3cca835c0aca108e68f3815c32cdd3dbc8eff5e5
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-02 16:30:41 +0100

    11. WS Standards Implement the handle method ...

--- 11. WS Standards/src/main/java/com/cleverdeveloper/soap/ws/config/UTPasswordCallBack.java
+++ 11. WS Standards/src/main/java/com/cleverdeveloper/soap/ws/config/UTPasswordCallBack.java
@@ -7,6 +7,8 @@ Author Name : @ DRRONIDZ
 DATE : 02/02/2022 16:20
 */
 
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
@@ -25,6 +27,14 @@ public class UTPasswordCallBack implements CallbackHandler {
 
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-
+        for (Callback callback: callbacks) {
+            // Getting the id (username) from the callback
+            WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+            // Setting the corresponding password to it
+            String password = usernamesPasswords.get(passwordCallback.getIdentifier());
+            if (password != null) {
+                passwordCallback.setPassword(password);
+            }
+        }
     }
 }
