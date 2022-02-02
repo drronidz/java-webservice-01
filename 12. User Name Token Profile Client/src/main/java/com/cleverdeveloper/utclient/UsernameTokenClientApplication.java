c4f6d5e7ebe0c2aa7ac8ef7f4a86c6a6b590078e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-02 21:24:06 +0100

    12. User Name Token Profile Client Creating the project ...

---
+++ 12. User Name Token Profile Client/src/main/java/com/cleverdeveloper/utclient/UsernameTokenClientApplication.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.utclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsernameTokenClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsernameTokenClientApplication.class, args);
+    }
+
+}
