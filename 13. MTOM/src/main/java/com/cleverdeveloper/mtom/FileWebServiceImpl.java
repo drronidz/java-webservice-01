7ab88642e2eaeb750e9ab76c16320def28d1705c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-03 00:22:18 +0100

    13. MTOM Test using soapUI . closes #7

--- 13. MTOM/src/main/java/com/cleverdeveloper/mtom/FileWebServiceImpl.java
+++ 13. MTOM/src/main/java/com/cleverdeveloper/mtom/FileWebServiceImpl.java
@@ -15,7 +15,7 @@ public class FileWebServiceImpl implements FileWebService{
     @Override
     public void upload(DataHandler attachment) throws IOException {
         InputStream inputStream = attachment.getInputStream();
-        OutputStream outputStream = new FileOutputStream(new File("C:/Users/DRRONIDZ/IdeaProjects/Java Vanilla/java-webservice-01/13. MTOM/uploaded/spring.jpg"));
+        OutputStream outputStream = new FileOutputStream(new File("C:\\Users\\DRRONIDZ\\IdeaProjects\\Java Vanilla\\java-webservice-01\\13. MTOM\\src\\main\\resources\\uploaded\\spring.jpg"));
 
         byte[] bytes = new byte[10000];
         int bytesRead = 0;
@@ -23,12 +23,15 @@ public class FileWebServiceImpl implements FileWebService{
         while ((bytesRead = inputStream.read(bytes))!= -1) {
             outputStream.write(bytes, 0, bytesRead);
         }
+
+        inputStream.close();
+        outputStream.close();
     }
 
     @Override
     public DataHandler download() {
         return new DataHandler(
                 new FileDataSource(
-                        new File("C:/Users/DRRONIDZ/IdeaProjects/Java Vanilla/java-webservice-01/13. MTOM/uploaded/spring.jpg")));
+                        new File("C:\\Users\\DRRONIDZ\\IdeaProjects\\Java Vanilla\\java-webservice-01\\13. MTOM\\src\\main\\resources\\uploaded\\spring.jpg")));
     }
 }
