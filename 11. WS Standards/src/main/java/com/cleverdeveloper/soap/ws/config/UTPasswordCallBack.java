0ab7913c5de93ea93c23ca370dab5246118530df
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-02 16:21:13 +0100

    11. WS Standards Providing the interceptor properties ...

---
+++ 11. WS Standards/src/main/java/com/cleverdeveloper/soap/ws/config/UTPasswordCallBack.java
@@ -0,0 +1,20 @@
+package com.cleverdeveloper.soap.ws.config;
+
+/*
+PROJECT NAME : 11. WS Standards
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 02/02/2022 16:20
+*/
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+public class UTPasswordCallBack implements CallbackHandler {
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+    }
+}
--- 11. WS Standards/src/main/java/com/cleverdeveloper/soap/ws/config/WebServiceConfig.java
+++ 11. WS Standards/src/main/java/com/cleverdeveloper/soap/ws/config/WebServiceConfig.java
@@ -11,6 +11,8 @@ import com.cleverdeveloper.soap.ws.PaymentProcessorImpl;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,9 +37,12 @@ public class WebServiceConfig {
         // Configuring the Interceptors ...
         // InInterceptor : to handle the incoming request ...
         // OutInterceptor: to handle the out coming request ...
-        Map<String, Object> hashMap = new HashMap<>();
+        Map<String, Object> inProps = new HashMap<>();
+        inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+        inProps.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+        inProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallBack.class.getName());
 
-        WSS4JInInterceptor inInterceptor = new WSS4JInInterceptor(hashMap);
+        WSS4JInInterceptor inInterceptor = new WSS4JInInterceptor(inProps);
         endpoint.getInInterceptors().add(inInterceptor);
 
         return endpoint;
