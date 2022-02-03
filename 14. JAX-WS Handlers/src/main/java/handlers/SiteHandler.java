11908e51a0c211a1c6ca80630e7d57a6774fdae7
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-03 14:20:27 +0100

    14. JAX-WS Handlers Creating the SiteHandler class. closes #12.

---
+++ 14. JAX-WS Handlers/src/main/java/handlers/SiteHandler.java
@@ -0,0 +1,36 @@
+package handlers;
+
+/*
+PROJECT NAME : 14. JAX-WS Handlers
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 03/02/2022 14:18
+*/
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.util.Set;
+
+public class SiteHandler implements SOAPHandler<SOAPMessageContext> {
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        return false;
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext context) {
+        return false;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+
+    }
+}
