4dedc69a3461b5b378125a02d076145c5ce9c77d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-03 14:53:34 +0100

    14. JAX-WS Handlers Configuring the handler. closes #15.

--- 14. JAX-WS Handlers/src/main/java/com/cleverdeveloper/ws/soap/topdown/config/WebServiceConfig.java
+++ 14. JAX-WS Handlers/src/main/java/com/cleverdeveloper/ws/soap/topdown/config/WebServiceConfig.java
@@ -8,13 +8,18 @@ DATE : 01/02/2022 22:01
 */
 
 import com.cleverdeveloper.ws.soap.topdown.CustomerOrdersWebServiceImpl;
+import handlers.SiteHandler;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.ws.Binding;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.soap.SOAPBinding;
+import java.util.ArrayList;
 
 @Configuration
 public class WebServiceConfig {
@@ -26,6 +31,13 @@ public class WebServiceConfig {
     public Endpoint endpoint() {
         Endpoint endpoint = new EndpointImpl(bus, new CustomerOrdersWebServiceImpl());
         endpoint.publish("/customerordersservice");
+
+        SOAPBinding binding = (SOAPBinding) endpoint.getBinding();
+        ArrayList<Handler> handlerChain = new ArrayList<>();
+        handlerChain.add(new SiteHandler());
+
+        binding.setHandlerChain(handlerChain);
+
         return endpoint;
     }
 }
