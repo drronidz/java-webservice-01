dd54eac0086e175c737598379d141365bd6cd45f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-05 13:40:42 +0100

    17. RESTFul Design and Implementation Creating the classes and annotations. closes #32

---
+++ 17. RESTful Design and Implementation/src/main/java/com/cleverdeveloper/restful/service/PatientService.java
@@ -0,0 +1,14 @@
+package com.cleverdeveloper.restful.service;
+
+/*
+PROJECT NAME : 17. RESTFul Design and Implementation
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 05/02/2022 13:36
+*/
+
+import javax.ws.rs.Path;
+
+@Path("/patientservice")
+public interface PatientService {
+}
---
+++ 17. RESTful Design and Implementation/src/main/java/com/cleverdeveloper/restful/service/PatientServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.restful.service;
+
+import org.springframework.stereotype.Service;
+
+/*
+PROJECT NAME : 17. RESTFul Design and Implementation
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 05/02/2022 13:37
+*/
+@Service
+public class PatientServiceImpl implements PatientService{
+}
