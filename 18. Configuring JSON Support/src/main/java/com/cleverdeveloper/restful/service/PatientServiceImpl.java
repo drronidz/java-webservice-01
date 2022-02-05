9f430b50232cadcbd0a6dac036276034eb16682e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-05 23:26:15 +0100

    18. Configure JSON Support:
    . closes #43

---
+++ 18. Configuring JSON Support/src/main/java/com/cleverdeveloper/restful/service/PatientServiceImpl.java
@@ -0,0 +1,80 @@
+package com.cleverdeveloper.restful.service;
+
+import com.cleverdeveloper.restful.model.Patient;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.*;
+
+/*
+PROJECT NAME : 17. RESTFul Design and Implementation
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 05/02/2022 13:37
+*/
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    Map<Long, Patient> patients = new HashMap<>();
+    long currentId = 0;
+
+    public PatientServiceImpl() {
+        init();
+    }
+
+    void init() {
+        Patient patient = new Patient();
+        patient.setId(++currentId);
+        patient.setName("John");
+
+        patients.put(patient.getId(), patient);
+
+    }
+
+    @Override
+    public List<Patient> getPatients() {
+        Collection<Patient> response = patients.values();
+        return new ArrayList(response);
+    }
+
+    @Override
+    public Patient getPatient(Long id) {
+        return patients.get(id);
+    }
+
+    @Override
+    public Response createPatient(Patient patient) {
+        patient.setId(++currentId);
+        patients.put(patient.getId(), patient);
+
+        return Response.ok(patient).build();
+    }
+
+    @Override
+    public Response updatePatient(Patient patient) {
+        Patient currentPatient = patients.get(patient.getId());
+
+        Response response;
+        if(currentPatient != null) {
+            patients.put(patient.getId(), patient);
+            response = Response.ok(patient).build();
+        } else {
+            response = Response.notModified().build();
+        }
+        return response;
+    }
+
+    @Override
+    public Response deletePatient(Long id) {
+        Patient patient = patients.get(id);
+        Response response;
+
+        if (patient != null) {
+            patients.remove(id);
+            response = Response.ok().build();
+        } else {
+            response = Response.notModified().build();
+        }
+        return response;
+    }
+}
