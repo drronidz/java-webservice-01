3d5e6269d481048c059f27a78ae27d62684050fd
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-05 13:48:39 +0100

    17. RESTFul Design and Implementation Implementing the init method. closes #33

--- 17. RESTful Design and Implementation/src/main/java/com/cleverdeveloper/restful/service/PatientServiceImpl.java
+++ 17. RESTful Design and Implementation/src/main/java/com/cleverdeveloper/restful/service/PatientServiceImpl.java
@@ -1,6 +1,10 @@
 package com.cleverdeveloper.restful.service;
 
+import com.cleverdeveloper.restful.model.Patient;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 PROJECT NAME : 17. RESTFul Design and Implementation
@@ -9,5 +13,21 @@ Author Name : @ DRRONIDZ
 DATE : 05/02/2022 13:37
 */
 @Service
-public class PatientServiceImpl implements PatientService{
+public class PatientServiceImpl implements PatientService {
+
+    Map<Long, Patient> patients = new HashMap<>();
+    long currentId = 0;
+
+    public PatientServiceImpl() {
+        init();
+    }
+
+    void init() {
+        Patient patient = new Patient();
+        patient.setId(++currentId);
+        patient.setName("John");
+
+        patients.put(patient.getId(), patient);
+
+    }
 }
