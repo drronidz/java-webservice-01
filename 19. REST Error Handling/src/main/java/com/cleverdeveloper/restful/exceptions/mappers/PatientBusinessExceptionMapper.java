178a9116d9b96af78b668a733b4d80c8f5b5dbfd
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-06 22:01:38 +0100

    19. REST Error Handling Creating the ExceptionMapper . closes #52.

---
+++ 19. REST Error Handling/src/main/java/com/cleverdeveloper/restful/exceptions/mappers/PatientBusinessExceptionMapper.java
@@ -0,0 +1,24 @@
+package com.cleverdeveloper.restful.exceptions.mappers;
+
+/*
+PROJECT NAME : 19. REST Error Handling
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 06/02/2022 21:58
+*/
+
+import com.cleverdeveloper.restful.exceptions.PatientBusinessException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class PatientBusinessExceptionMapper implements ExceptionMapper<PatientBusinessException> {
+
+
+    @Override
+    public Response toResponse(PatientBusinessException e) {
+        return Response.status(404).build();
+    }
+}
