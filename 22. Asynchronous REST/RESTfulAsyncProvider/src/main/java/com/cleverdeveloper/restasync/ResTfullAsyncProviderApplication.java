3426f3d6c1655b1f0449992543133fb8ed654653
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-09 18:48:37 +0100

    22. Asynchronous REST Creating the Async REST project. closes #77.

---
+++ 22. Asynchronous REST/RESTfulAsyncProvider/src/main/java/com/cleverdeveloper/restasync/ResTfullAsyncProviderApplication.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.restasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfullAsyncProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfullAsyncProviderApplication.class, args);
+    }
+
+}
