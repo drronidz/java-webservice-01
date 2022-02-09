37be0e03d753c867a796fa44e1d71b0c845d0d90
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-09 19:06:17 +0100

    22. Asynchronous REST Creating the Client project. closes #81.

---
+++ 22. Asynchronous REST/RESTfulAsyncClient/src/main/java/com/cleverdeveloper/restws/ResTfulAsyncClientApplication.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.restws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulAsyncClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulAsyncClientApplication.class, args);
+    }
+
+}
