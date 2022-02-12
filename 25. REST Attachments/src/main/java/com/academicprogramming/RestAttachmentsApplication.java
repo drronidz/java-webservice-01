e69428ee44b465f2f3632086717e12adc4bfeca0
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-12 14:56:07 +0100

    25. REST Attachments Creating the Project. closes #103.

---
+++ 25. REST Attachments/src/main/java/com/academicprogramming/RestAttachmentsApplication.java
@@ -0,0 +1,13 @@
+package com.academicprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAttachmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAttachmentsApplication.class, args);
+    }
+
+}
