712c921ea975969773c57fe3a725cff8e0db1a71
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-12 15:02:55 +0100

    25. REST Attachments Coding the Upload endpoint. closes #104.

--- 25. REST Attachments/src/main/java/com/academicprogramming/service/FileService.java
+++ 25. REST Attachments/src/main/java/com/academicprogramming/service/FileService.java
@@ -1,15 +1,29 @@
 package com.academicprogramming.service;
 
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
+@Path("/fileService")
 public class FileService {
 
-	private static final String FILE_PATH = "";
+	private static final String FILE_PATH = "C:\\Users\\DRRONIDZ\\IdeaProjects\\Java Vanilla\\java-webservice-01\\25. REST Attachments\\docs\\images";
+
+	@Path("/upload")
+	@POST
+	public void upload(List<Attachment> attachments) throws IOException {
+		for (Attachment attachment: attachments) {
+			copyFile(attachment.getDataHandler().getInputStream());
+		}
+	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
