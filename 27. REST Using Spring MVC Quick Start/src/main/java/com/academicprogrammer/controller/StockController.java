ef3f6cbfda5016f61bba5bb98dcc64e8865c2aba
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-14 17:32:17 +0100

    27. REST Using Spring MVC Quick Start Implementing Hello method in Spring REST. closes #117.

---
+++ 27. REST Using Spring MVC Quick Start/src/main/java/com/academicprogrammer/controller/StockController.java
@@ -0,0 +1,23 @@
+package com.academicprogrammer.controller;
+
+/*
+PROJECT NAME : springrest
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 14/02/2022 17:19
+*/
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class StockController {
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " to the RESTful Spring World";
+    }
+}
