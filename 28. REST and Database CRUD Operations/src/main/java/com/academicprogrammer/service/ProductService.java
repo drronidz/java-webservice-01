f709e11a34868413e511599922b872b7c7cd3085
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-02-15 14:29:00 +0100

    28. REST and Database CRUD Operations Using the JAX RS Annotations. closes #129.

--- 28. REST and Database CRUD Operations/src/main/java/com/academicprogrammer/service/ProductService.java
+++ 28. REST and Database CRUD Operations/src/main/java/com/academicprogrammer/service/ProductService.java
@@ -9,17 +9,29 @@ DATE : 15/02/2022 14:16
 
 import com.academicprogrammer.model.Product;
 
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Consumes("application/json")
+@Produces("application/json")
+@Path("/productservice/products")
 public interface ProductService {
 
+    @Path("")
+    @GET
     List<Product> getProducts();
 
-    Product getProduct(int id);
+    @Path("/{id}")
+    @GET
+    Product getProduct(@PathParam("id") int id);
 
+    @Path("/new")
+    @POST
     Response createProduct(Product product);
 
+    @Path("/update")
+    @PUT
     Response updateProduct(Product product);
 
 
